9b20c96c7a18e2c4e3c956efb78273f29d9a2bf1
Benson Chau <bc62101@uga.edu>
2016-11-19 19:24:07 +0000

    replaced nav bar with pull-out sidenav

---
+++ app/assets/javascripts/sitewide/main.js
@@ -0,0 +1,10 @@
+$(document).ready(function() {
+    var $toggleButton = $('.toggle-button'),
+        $menuWrap = $('.menu-wrap'); 
+
+    $toggleButton.on('click', function() {
+        $(this).toggleClass('button-open'); 
+        $menuWrap.toggleClass('menu-show');
+    });
+});
+
