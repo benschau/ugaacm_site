d8f48ebfee835742d1774582339bd9693c2044f9
Benson Chau <bc62101@uga.edu>
2017-01-06 00:23:26 +0000

    removed on-scroll animation

--- app/assets/javascripts/about/description.js
+++ app/assets/javascripts/about/description.js
@@ -1,14 +1,8 @@
 $(document).ready(function() {
-
-});
-
-$(window).scroll(function() {
     var hT = $('.descleft').offset().top,
         hH = $('.descleft').outerHeight(),
         wH = $(window).height(),
         wS = $(this).scrollTop();
-    if (wS > (hT + hH - wH)) {
-       $('.descleft').animate({left: '0px'}, 500);
-       $('.descright').animate({right: '0px'}, 500); 
-    }
+   $('.descleft').animate({left: '0px'}, 500);
+   $('.descright').animate({right: '0px'}, 500); 
 });
